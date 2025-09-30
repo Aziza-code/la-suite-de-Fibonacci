3c93981033bd4280ab5a390add4d4b0dfeec1642
Aziza-code <azizaosalh@gmail.com>
2025-09-30 23:16:08 +0100

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+def fib(n):
+    U0=0
+    U1=1
+    if n==0:
+        U=U0
+    elif n==1:
+        U=U1
+    else:
+        i=2
+        while(i<=n):
+            U=U1+U0
+            U0 = U1
+            U1=U
+            i+=1
+    return U
+print(fib(6))
